3506d6c7650ae851ba64c41190aecc600f7239dd
Daniel <danielgalt@yahoo.com>
2024-07-20 19:49:56 -0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import {useState} from "react";
+import Authenticate from "./components/Authenticate/Authenticate";
+import SignupForm from "./components/SignupForm/SignupForm";
+
+function App() {
+  const [token, setToken] = useState(null);
+  return (
+    <>
+      <Authenticate token={token}/>
+      <SignupForm setToken={setToken}/>
+    </>
+  );
+}
+
+export default App;
